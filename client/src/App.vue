dbc825a761c2998877aacae20794bf900323cba2
harshperson <harshperson@gmail.com>
2021-01-09 10:42:32 +0200

    12

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Todo application</h1>
+    <todoList :todos="todos"/>
+  </div>
+</template>
+
+<script>
+import todoList from './components/TodoList'
+export default {
+  name: 'App',
+  data(){
+    return{
+      todos:[
+        {id: 1, title: 'Bred', completed: false},
+        {id: 2, title: 'Milk', completed: false},
+        {id: 3, title: 'Voter', completed: false},
+      ]
+    }
+  },
+  components: {
+   todoList
+  }
+}
+</script>
